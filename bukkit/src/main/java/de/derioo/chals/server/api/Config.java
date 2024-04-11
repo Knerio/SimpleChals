b3c96b32250fc6cda53ea3e1ab9629f93fc4ccb0
Dario <derioreal63@gmail.com>
2024-04-11 17:23:24 +0200

    fix indentations

--- bukkit/src/main/java/de/derioo/chals/server/api/Config.java
+++ bukkit/src/main/java/de/derioo/chals/server/api/Config.java
@@ -11,56 +11,55 @@ import java.nio.charset.StandardCharsets;
 
 public class Config {
 
-  private JsonObject content;
-  private final Plugin plugin;
-  private final String name;
+    private JsonObject content;
+    private final Plugin plugin;
+    private final String name;
 
-  public Config(Plugin plugin, String name) {
-    this.plugin = plugin;
-    this.name = name;
-    new File("./plugins/sc/" + name).mkdirs();
-    File configFile = new File(new File("./plugins/sc/" + name), "config.json");
-    try {
-      if (configFile.createNewFile()) {
-        content = getDefault();
-      } else {
-        try (FileReader fileReader = new FileReader(configFile);) {
-          content = JsonParser.parseString(IOUtils.toString(fileReader)).getAsJsonObject();
-        } catch (Exception e) {
-          System.out.println("Found invalid json");
-          content = getDefault();
+    public Config(Plugin plugin, String name) {
+        this.plugin = plugin;
+        this.name = name;
+        new File("./plugins/sc/" + name).mkdirs();
+        File configFile = new File(new File("./plugins/sc/" + name), "config.json");
+        try {
+            if (configFile.createNewFile()) {
+                content = getDefault();
+            } else {
+                try (FileReader fileReader = new FileReader(configFile);) {
+                    content = JsonParser.parseString(IOUtils.toString(fileReader)).getAsJsonObject();
+                } catch (Exception e) {
+                    System.out.println("Found invalid json");
+                    content = getDefault();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+        save();
     }
-    save();
-  }
 
-  public JsonObject get() {
-    return content;
-  }
-
-  private JsonObject getDefault() throws IOException {
-    InputStream resource = plugin.getResource("config.json");
-    return JsonParser.parseString(IOUtils.toString(resource, StandardCharsets.UTF_8)).getAsJsonObject();
-  }
-
-  public void save() {
-    File dir = new File("./plugins/sc/" + name);
-    dir.mkdirs();
-    File configFile = new File(dir, "config.json");
-      try {
-          configFile.createNewFile();
-      } catch (IOException e) {
-          throw new RuntimeException(e);
-      }
-      try (BufferedWriter writer = new BufferedWriter(new FileWriter(configFile))) {
-      writer.write(content.toString());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    public JsonObject get() {
+        return content;
     }
-  }
+
+    private JsonObject getDefault() throws IOException {
+        InputStream resource = plugin.getResource("config.json");
+        return JsonParser.parseString(IOUtils.toString(resource, StandardCharsets.UTF_8)).getAsJsonObject();
+    }
+
+    public void save() {
+        File dir = new File(plugin.getDataFolder().getParentFile().toPath() + "/sc/" + name);
+        File configFile = new File(dir, "config.json");
+        try {
+            configFile.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(configFile))) {
+            writer.write(content.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
